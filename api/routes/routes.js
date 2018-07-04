1b2968b6614970033ceba949848b7faa56185830
Gabby <torres.gabriellemarie@gmail.com>
2018-07-04 13:41:57 +0800

    log xml in console

--- api/routes/routes.js
+++ api/routes/routes.js
@@ -1,6 +1,15 @@
+const axios = require('axios');
+
 var appRouter = function(app) {
     app.get("/", function(req,res) {
         res.status(200).send('YAY');
+        axios.get('https://www.up.edu.ph/index.php/feed/')
+        .then( (response) => {
+          console.log(response);  
+        })
+        .catch((err)=>{
+            console.log(err);
+        });
     });
 }
 

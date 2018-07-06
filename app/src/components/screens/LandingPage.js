286a6ce30fa1bfb6f5670c5b36d7f5566f1a42f1
Gabby <torres.gabriellemarie@gmail.com>
2018-07-06 17:51:26 +0800

    fetch data from landing page using service

--- app/src/components/screens/LandingPage.js
+++ app/src/components/screens/LandingPage.js
@@ -1,17 +1,28 @@
 import React, { Component } from 'react';
 import {View,Text} from 'react-native';
 import StorageService from '../../services/StorageService.js';
-const axios = require('axios');
+
 class LandingPage extends Component {
 
+    constructor() {
+        super();
+        this.storage = new StorageService();
+    }
+
     componentDidMount() {
-        {StorageService.getData()}
+        this.storage.getData();
     }
     render() {
+     //   var articlesItem = this.storage.articles.map((article)=> {
+       //     return( <Text>{article.title}</Text>);
+       // }); this is for printing titles
+       
         return(
             <View>
                 <Text>TEST</Text>
+                
             </View>
+           
         );
     }
 }
--- app/src/services/StorageService.js
+++ app/src/services/StorageService.js
@@ -1,17 +1,20 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 const axios = require('axios');
 
-class StorageService extends Component {
-    getData() {
-        axios.get('https://api.rss2json.com/v1/api.json?rss_url=https://www.up.edu.ph/index.php/feed/')
+class StorageService {
+    articles = [];
+    async getData() {
+        this.articles = [];
+        await axios.get('https://api.rss2json.com/v1/api.json?rss_url=https://www.up.edu.ph/index.php/feed/')
             .then( (response) => {
-              console.log(response.data);
+                let feed = response.data;
+                this.articles = feed.items;
             })
             .catch((err)=>{
                 console.log(err);
             });
+        console.log(this.articles);
     }
-
 }
 
-export default new StorageService();
+export default StorageService;

e979c186f4d404eac16b2566916ec6b9cdc5e865
Gabby <torres.gabriellemarie@gmail.com>
2018-07-11 16:00:59 +0800

    update gradle

--- app/index.js
+++ app/index.js
@@ -2,3 +2,5 @@ import { AppRegistry } from 'react-native';
 import App from './App';
 
 AppRegistry.registerComponent('app', () => App);
+import { YellowBox } from 'react-native';
+YellowBox.ignoreWarnings(['Warning: isMounted(...) is deprecated', 'Module RCTImageLoader']);

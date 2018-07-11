a61989fb5f8d68e350f64a89db3db7e89c29536f
Gabby <torres.gabriellemarie@gmail.com>
2018-07-11 10:07:01 +0800

    Revert "create bottom nav"
    This reverts commit 04c00aeb54b1c3ece56ea09e2c98c58f324b54f1.

--- app/src/components/navigation/BottomNav.js
+++ app/src/components/navigation/BottomNav.js
@@ -1,37 +1,13 @@
 import React from 'react';
 import {createBottomTabNavigator} from 'react-navigation';
-import Icon from 'react-native-vector-icons/Ionicons';
 
 import LandingPage from '../screens/LandingPage.js';
 import NewsPage from '../screens/NewsPage.js';
 
 export default BottomNav = createBottomTabNavigator({
-    Home: {
-        screen: LandingPage,
-        navigationOptions: {
-            tabBarLabel: 'Home',
-            tabBarIcon: ({tintColor}) => {
-                <Icon name='md-home' color={tintColor} size={24}/>
-            }
-        }
-    },
-    News: {
-        screen: NewsPage,
-        navigationOptions: {
-            tabBarLabel: 'News',
-            tabBarIcon: ({tintColor}) => {
-                <Icon name='md-paper' color={tintColor} size={24}/>
-            }
-        }
-    }
-}, {
-    initialRouteName: 'Home',
-    order: ['Home','News'],
-    navigationOptions: {
-        tabBarVisible: true
-    },
-    tabBarOptions: {
-        activeTintColor: 'red',
-        inactiveTintColor: 'grey'
-    }
-});
+    Home: LandingPage,
+    News: NewsPage,
+  } , {
+      swipeEnabled: true
+  }
+);
